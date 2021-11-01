9bbe2b1bac083f8c854c1acb47eab5c155584ec7
Ilyas Bezhanidze <iljas.bezhanidze@totonis.com>
2021-11-01 17:48:02 +0200

    create section reviews/skider

---
+++ src/js/vendor/swiper.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
