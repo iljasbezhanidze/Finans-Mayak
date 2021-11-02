ca0e96bc0f74bfdd6b827d49ce70fe5be6958363
Ilyas Bezhanidze <iljas.bezhanidze@totonis.com>
2021-11-02 13:05:01 +0200

    fonish sections contact, footer and PC version this suite

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,30 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
+// function anchor() {
+//     const links = document.querySelectorAll('a[href*=#]')
+//     for(elem of links) {
+//         elem.addEventListener('click', function(e) {
+//             e.preventDefault()
+
+//         })
+//     }
+// }
+
+function scrollTo(elem) {
+  window.scroll({
+    left: 0,
+    top: elem.offsetTop,
+    behavior: 'smooth'
+  });
+};
+
+document.querySelector('.b-scroll').addEventListener('click', () => {
+  scrollTo(document.querySelector('.b-contacts'))
+})
--- src/js/vendor/swiper.js
+++
@@ -1,8 +0,0 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
-    spaceBetween: 20,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
