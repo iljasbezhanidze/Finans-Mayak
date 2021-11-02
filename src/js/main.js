8d71e113de597bda8b3af0abf9a23161624df1f9
Ilyas Bezhanidze <iljas.bezhanidze@totonis.com>
2021-11-02 17:57:49 +0200

    connect smooth auto scroll to page

--- src/js/main.js
+++ src/js/main.js
@@ -7,24 +7,31 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-// function anchor() {
-//     const links = document.querySelectorAll('a[href*=#]')
-//     for(elem of links) {
-//         elem.addEventListener('click', function(e) {
-//             e.preventDefault()
-
-//         })
-//     }
-// }
-
-function scrollTo(elem) {
+function scrollTo(elem, correction = 0) {
   window.scroll({
     left: 0,
-    top: elem.offsetTop,
+    top: elem.offsetTop - correction,
     behavior: 'smooth'
   });
 };
 
-document.querySelector('.b-scroll').addEventListener('click', () => {
+const scrollBtnForm = document.querySelector('.b-btn__scroll'),
+      scrollBtnServices = document.querySelector('.b-menu__link_scroll-services'),
+      scrollbtnPrices = document.querySelector('.b-menu__link_scroll-prices'),
+      scrollBtnCompany = document.querySelector('.b-menu__link_scroll_about-company')
+
+scrollBtnForm.addEventListener('click', () => {
   scrollTo(document.querySelector('.b-main__section_light-background'))
+})
+
+scrollBtnServices.addEventListener('click', () => {
+  scrollTo(document.querySelector('.b-services'), 50)
+})
+
+scrollbtnPrices.addEventListener('click', () => {
+  scrollTo(document.querySelector('.b-stack__card'), 100)
+})
+
+scrollBtnCompany.addEventListener('click', () => {
+  scrollTo(document.querySelector('.b-benefits'), 50)
 })
